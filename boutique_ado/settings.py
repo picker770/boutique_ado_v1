a0c68386e6ab751099ee696aa69854d2fee176c1
picker770 <arehman897@gmail.com>
2026-05-30 16:01:44 +0100

    adjust setttings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -188,19 +188,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'crz5895-boutique-ado-v3' # change this to your AWS bucket name
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_STORAGE_BUCKET_NAME = 'crz5895-boutique-ado-v3'  # Your bucket name
+    AWS_S3_REGION_NAME = 'eu-north-1'  # You have this - good!
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-# Static and media files
+    
+    # Static and media files - THESE MUST BE INDENTED inside the if block
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
-
-# Override static and media URLs in production
+    
+    # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
