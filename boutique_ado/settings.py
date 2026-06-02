c36d73708637ae8eb06706615e4b22b20cb05df5
picker770 <arehman897@gmail.com>
2026-06-02 22:23:08 +0100

    sending real emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -214,12 +214,6 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 
-# -------------------------
-# EMAIL
-# -------------------------
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 # -------------------------
 # DEFAULT FIELD
@@ -244,5 +238,18 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
